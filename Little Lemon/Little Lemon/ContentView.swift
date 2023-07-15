9bb10654f08d4b620146634d3eed2e503c2b8076
Lefteris Kostakis <l.kostakis@afse.eu>
2023-07-15 14:20:44 +0300

    init SwiftUI

---
+++ Little Lemon/Little Lemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by Lefteris Kostakis on 15/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Lefteris Kostakis on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
