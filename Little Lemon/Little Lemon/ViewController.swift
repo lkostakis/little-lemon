7dbbb78617895feed40420967d5e545894231835
Lefteris Kostakis <l.kostakis@afse.eu>
2023-07-15 14:17:17 +0300

    LittleLemon init

---
+++ Little Lemon/Little Lemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Little Lemon
+//
+//  Created by Lefteris Kostakis on 15/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
